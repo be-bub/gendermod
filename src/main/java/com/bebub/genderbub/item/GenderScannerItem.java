e06eaed2db2fdee1e53139377345384a8d3aa990
Bebub <161068290+be-bub@users.noreply.github.com>
2026-04-12 04:05:25 +0300

    1.0.5

--- src/main/java/com/bebub/genderbub/item/GenderScannerItem.java
+++
@@ -1,9 +0,0 @@
-package com.bebub.genderbub.item;
-
-import net.minecraft.world.item.Item;
-
-public class GenderScannerItem extends Item {
-    public GenderScannerItem() {
-        super(new Item.Properties().stacksTo(1));
-    }
-}
